41466ec37256786ab4e60c1d5b142dbc77cc2982
hajimaru <hajime4515@gmail.com>
2025-03-16 12:29:39 +0900

    Revert "User authentication　追加"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  # usernameの登録・更新を許可とする記載
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
-  end
 end
