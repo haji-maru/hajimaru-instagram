eba978edae5ae5c92d0804963cd633da6fddd77f
hajimaru <hajime4515@gmail.com>
2025-03-18 06:31:59 +0900

    active_atrage  アイコンクリックで使用　JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,3 +14,19 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import $ from "jquery";
+import axios from "axios";
+import { csrfToken } from "rails-ujs";
+
+axios.defaults.headers.common["X-CSRF-Token"] = csrfToken(); // token発行 重要
+
+const image_selection = () => {
+  $("#avatar-preview").on("click", () => {
+    $("#avatar-input").click();
+  });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  image_selection();
+});
--- app/javascript/packs/profile.js
+++ app/javascript/packs/profile.js
@@ -3,3 +3,13 @@ import axios from "axios";
 import { csrfToken } from "rails-ujs";
 
 axios.defaults.headers.common["X-CSRF-Token"] = csrfToken(); // token発行 重要
+
+const image_selection = () => {
+  $("#avatar-preview").on("click", () => {
+    $("#avatar-input").click();
+  });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  image_selection();
+});
