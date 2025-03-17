bee4be67605e3620f4abefad5160b17941aaca5a
hajimaru <hajime4515@gmail.com>
2025-03-18 06:37:23 +0900

    update  記載修正

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -12,7 +12,7 @@ class ProfilesController < ApplicationController
 
   def update
     @profile = current_user.prepare_profile
-    @profile = assign_attributes(profile_params)
+    @profile.assign_attributes(profile_params)
     if @profile.save
       redirect_to profile_path, notice: '更新できました'
     else
