ce1056186ad407a7d218a8ad6dc9b17cd488f5a1
hajimaru <hajime4515@gmail.com>
2025-03-19 06:34:10 +0900

    profile.js に内容移動

--- app/javascript/packs/profile.js
+++ app/javascript/packs/profile.js
@@ -8,6 +8,14 @@ const image_selection = () => {
   $("#avatar-preview").on("click", () => {
     $("#avatar-input").click();
   });
+
+  $("#avatar-input").on("change", (event) => {
+    const fileInput = event.currentTarget; // イベントの対象要素を取得
+    if (fileInput.files && fileInput.files[0]) {
+      // サブミットボタンをクリック
+      $("#avatar-submit").click();
+    }
+  });
 };
 
 document.addEventListener("DOMContentLoaded", () => {
