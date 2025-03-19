9c499e90ab92f2c2bb00bb7aa498eacb42175942
hajimaru <hajime4515@gmail.com>
2025-03-20 07:29:29 +0900

    timeline_params 追加

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -10,15 +10,26 @@ class TimelinesController < ApplicationController
   end
 
   def new
+    @timeline = current_user.timelines.build
   end
 
   def create
+    @timeline = current_user.timelines.build(timeline_params)
+    if @timeline.save
+        redirect_to timelines_path, notice: '保存できました'
+    else
+      flash.now[:error] = '保存に失敗しました'
+    end
   end
 
   def destroy
   end
 
   private
+
+  def timeline_params
+    params.require(:timeline).permit(:content, :images)
+  end
 
   def set_timeline
     @profile = current_user.prepare_profile
