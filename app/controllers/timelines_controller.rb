481f1a940da7d9188fa07d0429088ce4c7f84b67
hajimaru <hajime4515@gmail.com>
2025-03-19 13:54:17 +0900

    timelines controller  追加

---
+++ app/controllers/timelines_controller.rb
@@ -0,0 +1,15 @@
+class TimelinesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
