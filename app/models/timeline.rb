a7b4465f4d52b35e0012596c9fffd4dc57869a0b
hajimaru <hajime4515@gmail.com>
2025-03-19 13:45:32 +0900

    timeline  モデル  作成

---
+++ app/models/timeline.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: timelines
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  posttime   :datetime
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_timelines_on_user_id  (user_id)
+#
+class Timeline < ApplicationRecord
+end
