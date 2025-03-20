9402cf97408dcf7f9887a51291507ed44cf08f89
hajimaru <hajime4515@gmail.com>
2025-03-20 14:04:21 +0900

    active_storage  ファイル指定　追加

--- app/javascript/packs/timeline.js
+++ app/javascript/packs/timeline.js
@@ -1,2 +1,20 @@
 import $ from "jquery";
 import axios from "modules/axios";
+
+const images_selection = () => {
+  $("#images-open").on("click", () => {
+    $("#images-select").click();
+  });
+
+  $("#images-select").on("change", (event) => {
+    const fileInput = event.currentTarget; // イベントの対象要素を取得
+    if (fileInput.files && fileInput.files[0]) {
+      // サブミットボタンをクリック
+      $("#avatar-submit").click();
+    }
+  });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  images_selection();
+});
