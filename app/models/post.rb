e35109c00a13495dcfc5935474baeabb87bf86f6
hajimaru <hajime4515@gmail.com>
2025-03-23 14:26:59 +0900

    timeline → post 移行

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,4 +13,10 @@
 #  index_posts_on_user_id  (user_id)
 #
 class Post < ApplicationRecord
+  belongs_to :user
+
+  validates :content, presence: true
+  validates :content, length: { minimum: 2, maximum: 100 }
+
+  has_many_attached :images
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
 
   resource :profile, only: [:show, :edit, :update]
 
-  resources :timelines, only: [:index, :show, :new, :create, :destroy]
+  resources :posts, only: [:index, :show, :new, :create, :destroy]
 end
