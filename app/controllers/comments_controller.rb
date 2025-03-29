a5c48474914c29a446e85b265fe8843ebc67683f
hajimaru <hajime4515@gmail.com>
2025-03-29 17:15:46 +0900

    投稿機能追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,5 +5,20 @@ class CommentsController < ApplicationController
   end
 
   def create
+    post = Post.find(params[:post_id])
+    @comment = post.comments.build(comment_params)
+    if @comment.save
+      redirect_to post_path(post), notice: 'コメントを追加'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :new
+    end
   end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
 end
