6acc10753709eb200726f83df6638521ecd1f07e
hajimaru <hajime4515@gmail.com>
2025-04-02 06:11:41 +0900

    jQuery フォーム表示

--- app/javascript/packs/comment.js
+++ app/javascript/packs/comment.js
@@ -2,13 +2,17 @@ import $ from "jquery";
 import axios from "modules/axios";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const post = $(".post-show");
-  const postId = post.data("post-id"); // 投稿ごとの post_id を取得
-
-  axios.get(`/posts/${postId}/comments`).then((response) => {
-    const comments = response.data;
-    comments.forEach((comment) => {
-      $(".comment-container").append(`<p>${comment.content}</p>`);
-    });
+  $(".show-comment-form").on("click", () => {
+    $(".show-comment-form").addClass("hidden");
+    $(".comment-area").removeClass("hidden");
   });
 });
+// const post = $(".post-show");
+// const postId = post.data("post-id"); // 投稿ごとの post_id を取得
+
+// axios.get(`/posts/${postId}/comments`).then((response) => {
+//   const comments = response.data;
+//   comments.forEach((comment) => {
+//     $(".comment-container").append(`<p>${comment.content}</p>`);
+//   });
+// });
