2ff2f19a6e8bbdbdd40f2581962ad52e1dff1c1a
hajimaru <hajime4515@gmail.com>
2025-04-02 06:18:21 +0900

    API 作成  @comment serializer で作られる

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,12 +15,9 @@ class CommentsController < ApplicationController
   def create
     post = Post.find(params[:post_id])
     @comment = post.comments.build(comment_params)
-    if @comment.save
-      redirect_to post_path(post), notice: 'コメントを追加'
-    else
-      flash.now[:error] = '更新できませんでした'
-      render :new
-    end
+    @comment.save!
+
+    render json: @comment
   end
 
   private
