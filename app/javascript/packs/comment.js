6c839cbce778e5318115c714b1fa25020ef3b7d7
hajimaru <hajime4515@gmail.com>
2025-04-02 06:19:55 +0900

    コメント送信のアクション定義　コメント内容の取得

--- app/javascript/packs/comment.js
+++ app/javascript/packs/comment.js
@@ -6,6 +6,10 @@ document.addEventListener("DOMContentLoaded", () => {
     $(".show-comment-form").addClass("hidden");
     $(".comment-area").removeClass("hidden");
   });
+
+  $(".add-comment-button").on("click", () => {
+    const content = $("#comment_content").val();
+  });
 });
 // const post = $(".post-show");
 // const postId = post.data("post-id"); // 投稿ごとの post_id を取得
