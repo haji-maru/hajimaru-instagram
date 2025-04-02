ceb66dc48392a0e3a6c147ee6fded606575f8ac4
hajimaru <hajime4515@gmail.com>
2025-04-03 06:17:32 +0900

    rollback して user と紐づけ

--- db/schema.rb
+++ db/schema.rb
@@ -38,10 +38,12 @@ ActiveRecord::Schema.define(version: 2025_03_27_201632) do
 
   create_table "comments", force: :cascade do |t|
     t.bigint "post_id", null: false
+    t.bigint "user_id", null: false
     t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
