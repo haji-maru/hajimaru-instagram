28a0d1d7e284482a6ed993f1830949ad1139163a
hajimaru <hajime4515@gmail.com>
2025-04-05 08:20:59 +0900

    コメントの追加

--- app/javascript/packs/comment.js
+++ app/javascript/packs/comment.js
@@ -2,6 +2,7 @@ import $ from "jquery";
 import axios from "modules/axios";
 
 document.addEventListener("DOMContentLoaded", () => {
+  // フォームの表示
   $(".show-comment-form").on("click", () => {
     $(".show-comment-form").addClass("hidden");
     $(".comment-area").removeClass("hidden");
@@ -9,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const dataSet = $("#post-show").data();
   const postId = dataSet.postId;
-
+  // comment_idに紐づくコメントを表示
   axios.get(`/posts/${postId}/comments`).then((response) => {
     const comments = response.data;
     comments.forEach((comment) => {
