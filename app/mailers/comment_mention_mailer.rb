d616798210764c8883c74be9089e576b0b516db8
hajimaru <hajime4515@gmail.com>
2025-04-07 06:27:04 +0900

    mailer メソッド修正

--- app/mailers/comment_mention_mailer.rb
+++ app/mailers/comment_mention_mailer.rb
@@ -1,6 +1,8 @@
 class CommentMentionMailer < ApplicationMailer
-  def account_name_mention(user)
-    @user = user
-    mail to: '@user.email', subject: '【お知らせ】メンションされました'
+  def account_name_mention(mention_user, comment)
+    @mention_user = mention_user # メンションされた人
+    @comment = comment # コメント内容
+    @post_url = post_url(@comment.post) # 投稿へのリンクを作成
+    mail to: @mention_user.email, subject: '【お知らせ】メンションされました'
   end
 end
