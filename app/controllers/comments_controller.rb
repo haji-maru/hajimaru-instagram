05d142570f8e072105dad6cb3a7b6cc02a5f0143
hajimaru <hajime4515@gmail.com>
2025-04-07 06:26:37 +0900

    メール送付のメソッド移動

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -26,16 +26,4 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:content)
   end
-
-  def check_mentions
-    # メンションされたユーザーの検出
-    mentioned_usernames = @comment.content.scan(/@(\w+)/).flatten
-    # メンションされたユーザーの取得
-    mentioned_users = User.where(username: mentioned_usernames)
-    # 通知メールの送信
-    mentioned_users.each do |mention_user|
-      CommentMentionMailer.account_name_mention(mention_user, @comment).deliver_later
-    end
-  end
-
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -18,6 +18,18 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
 
-
   validates :content, presence: true
+
+  private
+
+  def check_mentions
+    # メンションされたユーザーの検出
+    mentioned_usernames = @comment.content.scan(/@(\w+)/).flatten
+    # メンションされたユーザーの取得
+    mentioned_users = User.where(username: mentioned_usernames)
+    # 通知メールの送信
+    mentioned_users.each do |mention_user|
+      CommentMentionMailer.account_name_mention(mention_user, @comment).deliver_later
+    end
+  end
 end
