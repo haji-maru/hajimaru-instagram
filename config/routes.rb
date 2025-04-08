fdebf5b56798d982c3db878c9ca0944b172dcc71
hajimaru <hajime4515@gmail.com>
2025-04-09 05:30:51 +0900

    sidekiq  設定  追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq' if Rails.env.development?
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
