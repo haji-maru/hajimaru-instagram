eeab041af7aa0c572d53d8e01705cd88d570706e
hajimaru <hajime4515@gmail.com>
2025-04-10 05:53:12 +0900

    relationship モデル  追加

---
+++ app/models/relationship.rb
@@ -0,0 +1,10 @@
+# == Schema Information
+#
+# Table name: relationships
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Relationship < ApplicationRecord
+end
