1df869256781ae2e0a935296a9b06c6cd37ddaaa
hajimaru <hajime4515@gmail.com>
2025-04-10 06:04:10 +0900

    follower_id 外部キー設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,6 +33,8 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+
   def prepare_profile
     profile || build_profile
   end
