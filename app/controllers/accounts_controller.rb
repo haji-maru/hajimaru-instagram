7b5811d7c49a379e22ea7aadc9c5b722b212c3d3
hajimaru <hajime4515@gmail.com>
2025-04-12 07:04:38 +0900

    ログインしているユーザーはaccountページからprofileページにリダイレクト

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,5 +2,8 @@ class AccountsController < ApplicationController
   def show
     @user = User.find(params[:id])
     @profile = @user.prepare_profile
+    if @user == current_user
+      redirect_to profile_path
+    end
   end
 end
