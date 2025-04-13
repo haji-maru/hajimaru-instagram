a1ce15066ee01523484de36bf8a798cf1baf1f60
hajimaru <hajime4515@gmail.com>
2025-04-13 15:28:01 +0900

    Ajax で渡せるように following と follower カウント 追加

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -8,8 +8,9 @@ class FollowsController < ApplicationController
   end
 
   def create
+    user = User.find(params[:account_id])
     current_user.follow!(params[:account_id])
 
-    render json: { status: 'ok' }
+    render json: { status: 'ok', follower_count: user.follower_count, following_count: user.following_count }
   end
 end
