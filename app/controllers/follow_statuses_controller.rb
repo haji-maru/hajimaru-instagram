a79d5c0a691198e5b24255b080cdfb95426a47fd
hajimaru <hajime4515@gmail.com>
2025-04-13 16:04:44 +0900

    follow_statuses controller 追加

---
+++ app/controllers/follow_statuses_controller.rb
@@ -0,0 +1,9 @@
+class FollowStatusesController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    user = User.find(params[:account_id])
+    follow_status = current_user.has_followed?(user)
+    render json: { hasFollowed: follow_status }
+  end
+end
