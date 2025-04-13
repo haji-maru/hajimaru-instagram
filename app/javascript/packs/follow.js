0d797b64fdc53edcc6c5883084902057c959a53f
hajimaru <hajime4515@gmail.com>
2025-04-13 11:52:16 +0900

    data status 取得 追加

--- app/javascript/packs/follow.js
+++ app/javascript/packs/follow.js
@@ -1,4 +1,11 @@
 import $ from "jquery";
 import axios from "modules/axios";
 
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  const dataSet = $("#follow-show").data();
+  const accountId = dataSet.accountId;
+
+  axios.get(`/accounts/${accountId}/follow`).then((response) => {
+    console.log(response);
+  });
+});
