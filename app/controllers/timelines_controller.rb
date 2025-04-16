ca44a36905804bb11e0c88e161dc7d16f7148e79
hajimaru <hajime4515@gmail.com>
2025-04-17 05:53:06 +0900

    () にするところを [] になっていたので修正

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -2,7 +2,7 @@ class TimelinesController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    user_ids = current_user.followings.pluck[:id]
+    user_ids = current_user.followings.pluck(:id)
     @posts = Post.where(user_id: user_ids)
   end
 end
