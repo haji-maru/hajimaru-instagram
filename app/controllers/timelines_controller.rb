7c42742af79f4382a82e9443bdb6f20cd25ed417
hajimaru <hajime4515@gmail.com>
2025-04-18 06:34:54 +0900

    コメントの修正

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -7,7 +7,7 @@ class TimelinesController < ApplicationController
     .where(user_id: user_ids) # フォローしているユーザーの投稿取得
     .where('posts.created_at >= ?', 24.hours.ago) # 24時間以内に作られた投稿
     .left_joins(:likes) # いいねされてない投稿も含む joins(:likes) いいねされてる投稿のみ
-    .group('posts.id') # post.idをグループ化
+    .group('posts.id') # posts.idをグループ化
     .order('COUNT(likes.id) DESC') # いいね数が多い順に並べる
     .limit(5) # 上位5件を取得
   end
