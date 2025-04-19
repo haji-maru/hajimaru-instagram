c492529d9405f0eeb49cf3cd87c939fb867a22c7
hajimaru <hajime4515@gmail.com>
2025-04-19 17:45:57 +0900

    rollback して profile 修正

--- db/migrate/20250316040242_create_profiles.rb
+++ db/migrate/20250316040242_create_profiles.rb
@@ -2,7 +2,6 @@ class CreateProfiles < ActiveRecord::Migration[6.0]
   def change
     create_table :profiles do |t|
       t.references :user, null: false
-      t.string :username
       t.timestamps
     end
   end
