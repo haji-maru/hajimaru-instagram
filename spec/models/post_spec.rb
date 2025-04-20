c9e53b03cb8bc39d00d4eb44855a6301767dcad5
hajimaru <hajime4515@gmail.com>
2025-04-21 06:49:35 +0900

    post spec テスト 1つ目 追加

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'タイトルと画像が入力されていれば、投稿が保存できる' do
+    user = User.create!({
+      email: 'test@example.com',
+      password: 'password',
+      username: 'test'
+    })
+    post = user.posts.build({
+      content: Faker::Lorem.characters(number: 10)
+    })
+
+    expect(post).to be_valid
+  end
 end
