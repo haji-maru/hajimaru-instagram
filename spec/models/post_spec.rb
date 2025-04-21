18ae72922ae3f45d0c6bb15f065a62fff2ca8c16
hajimaru <hajime4515@gmail.com>
2025-04-22 05:31:43 +0900

    context before 追加

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it 'タイトルと画像が入力されていれば、投稿が保存できる' do
-    user = User.create!({
-      email: 'test@example.com',
-      password: 'password',
-      username: 'test'
-    })
-    post = user.posts.build({
-      content: Faker::Lorem.characters(number: 10)
-    })
+  context 'タイトルと画像が入力されている場合' do
+    before do
+      user = User.create!({
+        email: 'test@example.com',
+        password: 'password',
+        username: 'test'
+      })
+      @post = user.posts.build({
+        content: Faker::Lorem.characters(number: 10)
+      })
+    end
 
-    expect(post).to be_valid
+    it '投稿が保存できる' do
+      expect(@post).to be_valid
+    end
   end
 end
