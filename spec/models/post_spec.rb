9b1c3aebe54f1fcdfd592e411c76e6c498077a83
hajimaru <hajime4515@gmail.com>
2025-04-22 06:35:18 +0900

    保存できな時のテスト 追加

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  context 'タイトルと画像が入力されている場合' do
-    let!(:user) do
-      User.create!({
-        email: 'test@example.com',
-        password: 'password',
-        username: 'test'
-      })
-    end
+  let!(:user) do
+    User.create!({
+      email: 'test@example.com',
+      password: 'password',
+      username: 'test'
+    })
+  end
 
+  context 'タイトルと画像が入力されている場合' do
     let!(:post) do
       user.posts.build({
         content: Faker::Lorem.characters(number: 10)
@@ -18,6 +18,18 @@ RSpec.describe Post, type: :model do
 
     it '投稿が保存できる' do
       expect(post).to be_valid
+    end
+  end
+
+  context 'タイトルの文字が一文字の場合' do
+    let!(:post) do
+      user.posts.create({
+        content: Faker::Lorem.characters(number: 1)
+      })
+    end
+
+    it '投稿を保存できない' do
+      expect(post.errors.messages[:content][0]).to eq('は2文字以上で入力してください')
     end
   end
 end
