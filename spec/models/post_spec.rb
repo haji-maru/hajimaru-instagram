14e2256bbb46146327a973ecdf828e3417461c5a
hajimaru <hajime4515@gmail.com>
2025-04-22 05:35:16 +0900

    let に変更

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,19 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   context 'タイトルと画像が入力されている場合' do
-    before do
-      user = User.create!({
+    let!(:user) do
+      User.create!({
         email: 'test@example.com',
         password: 'password',
         username: 'test'
       })
-      @post = user.posts.build({
+    end
+
+    let!(:post) do
+      user.posts.build({
         content: Faker::Lorem.characters(number: 10)
       })
     end
 
     it '投稿が保存できる' do
-      expect(@post).to be_valid
+      expect(post).to be_valid
     end
   end
 end
