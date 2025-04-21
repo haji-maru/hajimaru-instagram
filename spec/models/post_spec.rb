88f7c801c6541bf35360671a94a201ee7972da3b
hajimaru <hajime4515@gmail.com>
2025-04-22 07:04:37 +0900

    post model テスト追加

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,11 +4,7 @@ RSpec.describe Post, type: :model do
   let!(:user) { create(:user) }
 
   context 'タイトルと画像が入力されている場合' do
-    let!(:post) do
-      user.posts.build({
-        content: Faker::Lorem.characters(number: 10)
-      })
-    end
+    let!(:post) { build(:post, user: user) }
 
     it '投稿が保存できる' do
       expect(post).to be_valid
@@ -16,10 +12,10 @@ RSpec.describe Post, type: :model do
   end
 
   context 'タイトルの文字が一文字の場合' do
-    let!(:post) do
-      user.posts.create({
-        content: Faker::Lorem.characters(number: 1)
-      })
+    let!(:post) { build(:post, content: Faker::Lorem.characters(number: 1), user: user) }
+
+    before do
+      post.save
     end
 
     it '投稿を保存できない' do
