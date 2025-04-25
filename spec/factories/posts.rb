d336612900f6931578f69ff54ae6ed1f89eee853
hajimaru <hajime4515@gmail.com>
2025-04-25 14:04:38 +0900

    factory_bot 画像添付できるように追加

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :post do
     content { Faker::Lorem.characters(number: 50) }
+
+    after(:build) do |post|
+      post.image.attach(
+        io: File.open(Rails.root.join('spec/fixtures/test.jpg')),
+        filename: 'test.jpg',
+        content_type: 'image/jpeg'
+      )
+    end
   end
 end
