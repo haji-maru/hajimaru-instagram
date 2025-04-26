bb7724842979ee754f6cbea9e0acdcf1687b8adc
hajimaru <hajime4515@gmail.com>
2025-04-26 12:56:31 +0900

    投稿内容と画像が保存できるかテスト 追加

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -16,12 +16,23 @@ RSpec.describe 'Posts', type: :request do
   end
 
   describe 'POST /posts' do
-    it '投稿が保存される' do
-      post_params = attributes_for(:post)
-      post posts_path(post: post_params)
-      expect(response).to have_http_status(302)
-      expect(Post.last.content).to eq(post_params[:content])
-      # expect(Post.last.content).to eq(post_params[images: []])
+    let!(:image_path) { Rails.root.join('app/assets/images/back-icon.png') }
+    let!(:image_file) { fixture_file_upload(image_path, 'image/png') }
+
+    context '投稿内容と画像の選択が行われている場合' do
+      it '投稿と画像が保存される' do
+        post_params = {
+          content:Faker::Lorem.characters(number: 20),
+          images: [image_file]
+        }
+
+        post posts_path, params: { post: post_params }
+
+        expect(response).to have_http_status(302)
+        expect(Post.last.content).to eq(post_params[:content])
+        expect(Post.last.images.attached?).to be true
+        expect(Post.last.images.first.filename.to_s).to eq('back-icon.png')
+      end
     end
   end
 end
