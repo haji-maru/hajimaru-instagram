f160d1d3dee52076b653179495ba0f11b8212ad5
hajimaru <hajime4515@gmail.com>
2025-04-26 16:13:27 +0900

    image から images に変更

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,9 +2,9 @@ FactoryBot.define do
   factory :post do
     content { Faker::Lorem.characters(number: 50) }
 
-    trait :with_image do
+    trait :with_images do
       after(:build) do |post|
-        post.image.attach(
+        post.images.attach(
           io: File.open(Rails.root.join('app/assets/images/back-icon.png')),
           filename: 'back-icon.png',
           content_type: 'image/png'
