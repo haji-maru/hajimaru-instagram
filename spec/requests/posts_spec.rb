98a58cb33de3625c79ea9207003f295a922c24e0
hajimaru <hajime4515@gmail.com>
2025-04-26 16:15:32 +0900

    images に修正

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   let!(:user) { create(:user) }
+  let!(:posts) { create_list(:post, 3, :with_images, user: user) }
 
   before do
     sign_in user
