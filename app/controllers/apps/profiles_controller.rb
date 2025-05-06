bab556b2b410aaa676a5bb3e5437e8236a3e3d85
hajimaru <hajime4515@gmail.com>
2025-05-07 06:39:37 +0900

    失敗時の挙動をjson形式に変更

--- app/controllers/apps/profiles_controller.rb
+++ app/controllers/apps/profiles_controller.rb
@@ -10,8 +10,7 @@ class Apps::ProfilesController < Apps::ApplicationController
     if @profile.save
       render json: { status: 'ok', message: '更新できました', avatar_url: url_for(@profile.avatar) }
     else
-      flash.now[:error] = '更新できませんでした'
-      render :edit
+      render json: { status: 'error', message: '更新できませんでした', errors: @profile.errors.full_messages}, status: :unprocessable_entity
     end
   end
 
