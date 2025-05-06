7b92d6f993612f20c8d2bf4ca17c1e9f8643dcf6
hajimaru <hajime4515@gmail.com>
2025-05-06 16:05:25 +0900

    comments controller createアクション テスト 追加

--- spec/requests/api/comments_spec.rb
+++ spec/requests/api/comments_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Api::Comments', type: :request do
   let!(:user) { create(:user) }
-  let!(:post) { create(:post, :with_images,  user: user) }
-  let!(:comments) { create_list(:comment, 3, post: post, user: user) }
+  let!(:test_post) { create(:post, :with_images,  user: user) }
+  let!(:comments) { create_list(:comment, 3, post: test_post, user: user) }
 
   before do
     sign_in user
@@ -12,7 +12,7 @@ RSpec.describe 'Api::Comments', type: :request do
   context 'indexアクションにリクエストを送った場合' do
     describe 'GET /api/comments' do
       it '200ステータスが返ってくる' do
-        get api_comments_path(post_id: post.id)
+        get api_comments_path(post_id: test_post.id)
         expect(response).to have_http_status(200)
 
         body = JSON.parse(response.body)
@@ -20,6 +20,22 @@ RSpec.describe 'Api::Comments', type: :request do
         expect(body[0]['content']).to eq comments.first.content
         expect(body[1]['content']).to eq comments.second.content
         expect(body[2]['content']).to eq comments.third.content
+      end
+    end
+  end
+
+  context 'createアクションにリクエストを送った場合' do
+    describe 'POST /api/comments' do
+      it '200ステータスが返ってくる' do
+        comment_params = {
+          content:Faker::Lorem.characters(number: 10),
+        }
+
+        post api_comments_path(post_id: test_post.id), params: { comment: comment_params }
+        expect(response).to have_http_status(200)
+
+        body = JSON.parse(response.body)
+        expect(body['content']).to eq comment_params[:content]
       end
     end
   end
