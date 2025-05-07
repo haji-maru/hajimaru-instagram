4ecbee9f229210b9c2b673967eb95fda9a52a368
hajimaru <hajime4515@gmail.com>
2025-05-08 05:46:04 +0900

    profile 200ステータスのテスト 追加

---
+++ spec/requests/profiles_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe 'Profiles', type: :request do
+  let!(:user) { create(:user) }
+  let!(:profile) { create(:profile, :with_avatar, user: user) }
+
+  before do
+    sign_in user
+  end
+
+  describe 'GET /profile' do
+    context 'プロフィールページに遷移した場合' do
+      it '200ステータスが返ってくる' do
+        get profile_path
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+
+  describe 'PATCH /profile' do
+    context 'プロフィール画像を更新した場合' do
+      let!(:image_path) { Rails.root.join('app/assets/images/back-icon.png') }
+      let!(:avatar_file) { fixture_file_upload(image_path, 'image/png') }
+
+      it '200ステータスが返ってくる、ステータス・メッセージ・URLが返ってくる' do
+        patch profile_path, params: { profile: { avatar: avatar_file } }
+        expect(response).to have_http_status(200)
+
+        body = JSON.parse(response.body)
+        expect(body['status']).to eq ('ok')
+        expect(body['message']).to eq ('更新できました')
+        expect(body['avatar_url']).to include('back-icon.png')
+      end
+    end
+  end
+end
