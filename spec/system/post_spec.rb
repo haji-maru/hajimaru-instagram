9c0811a2e0440f2fa7ca0606e5f629bf8a9ba022
hajimaru <hajime4515@gmail.com>
2025-05-11 11:05:05 +0900

    context 追加

--- spec/system/post_spec.rb
+++ spec/system/post_spec.rb
@@ -4,39 +4,41 @@ RSpec.describe 'Post', type: :system do
   let!(:user) { create(:user) }
   let!(:posts) { create_list(:post, 3, :with_images, user: user) }
 
-  before do
-    sign_in user
-  end
-
-  it '投稿一覧が表示される' do
-    visit root_path
-    posts.each do |post|
-      expect(page).to have_css('.post-comments', text: post.content)
+  context 'ログインしている場合' do
+    before do
+      sign_in user
     end
-  end
 
-  it '吹き出しアイコンを押したらコメント一覧が表示される' do
-    visit root_path
-    all('.comment-button').first.click
-    expect(page).to have_css('.post-comment-title', text: 'Comment')
-  end
+    it '投稿一覧が表示される' do
+      visit root_path
+      posts.each do |post|
+        expect(page).to have_css('.post-comments', text: post.content)
+      end
+    end
 
-  it 'アバターアイコンを押したらプロフィールが表示される' do
-    visit root_path
-    all('.post-avatar').first.click
-    expect(page).to have_css('.profile-username', text: user.username)
-  end
+    it '吹き出しアイコンを押したらコメント一覧が表示される' do
+      visit root_path
+      all('.comment-button').first.click
+      expect(page).to have_css('.post-comment-title', text: 'Comment')
+    end
 
-  it 'Timelineを押したらタイムラインが表示される' do
-    visit root_path
-    click_link('Timeline')
-    expect(page).to have_current_path(timeline_path)
-  end
+    it 'アバターアイコンを押したらプロフィールが表示される' do
+      visit root_path
+      all('.post-avatar').first.click
+      expect(page).to have_css('.profile-username', text: user.username)
+    end
 
-  it 'カメラアイコンを押したら投稿ページが表示される' do
-    visit root_path
-    find('.post-create-icon').click
-    expect(page).to have_current_path(new_post_path)
-    expect(page).to have_css('.post-username', text: user.username)
+    it 'Timelineを押したらタイムラインが表示される' do
+      visit root_path
+      click_link('Timeline')
+      expect(page).to have_current_path(timeline_path)
+    end
+
+    it 'カメラアイコンを押したら投稿ページが表示される' do
+      visit root_path
+      find('.post-create-icon').click
+      expect(page).to have_current_path(new_post_path)
+      expect(page).to have_css('.post-username', text: user.username)
+    end
   end
 end
