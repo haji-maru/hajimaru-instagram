506b2c4465c435b3bc1cc1e55c7dfe09fc4ea27e
hajimaru <hajime4515@gmail.com>
2025-05-13 07:08:43 +0900

    本番環境では .env を読み込まないように修正

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,10 @@ module HajimaruInstagram
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    Bundler.require(*Rails.groups)
-    Dotenv::Railtie.load if %w[development test].include? ENV['RAILS_ENV']
+    if Rails.env.development? || Rails.env.test?
+      Bundler.require(*Rails.groups)
+      Dotenv::Railtie.load if %w[development test].include? ENV['RAILS_ENV']
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
