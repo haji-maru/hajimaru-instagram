8e54f4213b4aabb116c495ab903ff62a782389ab
hajimaru <hajime4515@gmail.com>
2025-05-17 07:11:26 +0900

    重複部分を削除

--- babel.config.js
+++ babel.config.js
@@ -79,12 +79,6 @@ module.exports = function (api) {
           loose: true,
         },
       ],
-      [
-        "@babel/plugin-transform-private-property-in-object",
-        {
-          loose: true,
-        },
-      ],
     ].filter(Boolean),
   };
 };
